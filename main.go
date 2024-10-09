ec9635a03b96d2a61fbacaa95f992cda96289c43
Eric Yue <yuekewin@gmail.com>
2024-10-09 11:12:12 +0800

    feat: add yaml2json function and testcases

--- main.go
+++ main.go
@@ -27,3 +27,21 @@ func json2yaml(data []byte) ([]byte, error) {
 
 	return out, nil
 }
+
+func yaml2json(data []byte) ([]byte, error) {
+	var tmp map[string]any
+
+	err := yaml.Unmarshal(data, &tmp)
+	if err != nil {
+		log.Println("yaml unmarshal error:", err)
+		return nil, err
+	}
+
+	out, err := jsoniter.MarshalIndent(&tmp, "", "  ")
+	if err != nil {
+		log.Println("json marshal error:", err)
+		return nil, err
+	}
+
+	return out, nil
+}
--- main_test.go
+++ main_test.go
@@ -8,20 +8,46 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-//go:embed testdata/json1.json
+//go:embed testdata/json/json1.json
 var json1 []byte
 
-//go:embed testdata/json2.json
+//go:embed testdata/json/json2.json
 var json2 []byte
 
-//go:embed testdata/json_all_type.json
+//go:embed testdata/json/json_all_type.json
 var jsonAllType []byte
 
 func Test_json2yaml(t *testing.T) {
-	// got, err := json2yaml(json1)
-	// got, err := json2yaml(json2)
-	got, err := json2yaml(jsonAllType)
-	assert.NoError(t, err)
-	assert.NotEmpty(t, got)
-	fmt.Println(string(got))
+	test := func(data []byte) {
+		got, err := json2yaml(data)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, got)
+		fmt.Println(string(got))
+	}
+
+	test(json1)
+	test(json2)
+	test(jsonAllType)
+}
+
+//go:embed testdata/yaml/yaml1.yaml
+var yaml1 []byte
+
+//go:embed testdata/yaml/yaml2.yaml
+var yaml2 []byte
+
+//go:embed testdata/yaml/yaml_all_type.yaml
+var yamlAllType []byte
+
+func Test_yaml2json(t *testing.T) {
+	test := func(data []byte) {
+		got, err := yaml2json(data)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, got)
+		fmt.Println(string(got))
+	}
+
+	test(yaml1)
+	test(yaml2)
+	test(yamlAllType)
 }
