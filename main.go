bb9c423dc687320686d0c64eb330a57dfe90d4bd
Eric Yue <yuekewin@gmail.com>
2024-10-10 11:29:07 +0800

    feat(static,main.go): add web page, add http router

--- main.go
+++ main.go
@@ -1,13 +1,69 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
+	"net/http"
 
 	jsoniter "github.com/json-iterator/go"
 	"gopkg.in/yaml.v3"
 )
 
 func main() {
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/json2yaml", json2yamlHandler)
+	http.HandleFunc("/yaml2json", yaml2jsonHandler)
+	fmt.Println("starting server on :21111")
+	if err := http.ListenAndServe(":21111", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func json2yamlHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("failed to read request body, error:%v", err)
+		http.Error(w, "failed to read request body", http.StatusBadRequest)
+		return
+	}
+
+	bytes, err := json2yaml(body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/x-yaml")
+	_, _ = w.Write(bytes)
+}
+
+func yaml2jsonHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("failed to read request body, error:%v", err)
+		http.Error(w, "failed to read request body", http.StatusBadRequest)
+		return
+	}
+
+	bytes, err := yaml2json(body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, _ = w.Write(bytes)
 }
 
 func json2yaml(data []byte) ([]byte, error) {
